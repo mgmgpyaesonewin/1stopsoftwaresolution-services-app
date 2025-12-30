ea9ad84f391686b1ec48e0b485cbb06325e31e60
Pyae Sone Win <pyaesonewin.dev@gmail.com>
2025-12-30 12:32:55 +0630

    chore: update base URL in robots and sitemap configurations

--- app/robots.ts
+++ app/robots.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = 'https://1stopsoftwaresolution.com'
+  const baseUrl = 'https://www.1stopsoftwaresolution.services'
 
   return {
     rules: [
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://1stopsoftwaresolution.com'
+  const baseUrl = 'https://www.1stopsoftwaresolution.services'
 
   return [
     {
